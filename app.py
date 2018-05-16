67fbe852e3eb61bb5ec7d3846996866f51b547aa
NightKirie <qwer55113322@gmail.com>
2018-05-17 00:39:16 +0800

    test

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text='lolololol')
+    message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(
         event.reply_token,
         message)
