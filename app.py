fd298b2ff40f14cffb4f55b22c4b7d08f55dcd15
NightKirie <qwer55113322@gmail.com>
2018-05-16 23:02:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python3  
+# -*- coding: utf-8 -*-  
+"""  
+@desc:  
+@author: TsungHan Yu  
+@contact: nick.yu@hzn.com.tw  
+@software: PyCharm  @since:python 3.6.0 on 2017/7/13
+"""
+
+import os
+import requests
 from flask import Flask, request, abort
 
 from linebot import (
@@ -7,17 +18,18 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
+    MessageEvent, TextMessage, TextSendMessage,
 )
 
 app = Flask(__name__)
 
-# Channel Access Token
-line_bot_api = LineBotApi('Ie/Zd3EYV5q9mwQjAuEGe2xkjuBH25g0hlzzOu2B+WOFRrluCuvowKaZqmlAN+KMzQkYWA8yiFFjSPN+uuiTYMhU0HNioAv7PqNqTQk14oBZRD/aYQ46faf16HziOZghcxWuYjOXZIZM1jnBMMYdewdB04t89/1O/w1cDnyilFU=')
-# Channel Secret
-handler = WebhookHandler('2851d4db0aa44fce3cdfd5e471cf2f98')
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+SECRET = os.environ.get('SECRET')
 
-# 監聽所有來自 /callback 的 Post Request
+line_bot_api = LineBotApi(ACCESS_TOKEN)
+handler = WebhookHandler(SECRET)
+
+
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -38,12 +50,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text='Hello World')
+    res = event.message.text
     line_bot_api.reply_message(
         event.reply_token,
-        'Hello World')
+        TextSendMessage(text=res))
 
-import os
+
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
