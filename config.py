1b8fba4b249c542f035eebde5c8219eb496f6c97
NightKirie <qwer55113322@gmail.com>
2018-05-20 11:57:37 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # imgur key
-client_id = 'YOUR_IMGUR_CLIENT_ID'
-client_secret = 'YOUR_IMGUR__CLIENT_SECRET'
-album_id = 'YOUR_IMGUR_ALBUM_ID'
-access_token = 'YOUR_IMGUR_ACCESS_TOKEN'
-refresh_token = 'YOUR_IMGUR_ACCESS_TOKEN'
+client_id = 'e69de1530215823'
+client_secret = 'fa632f78ea2b97350f980b121e29f0ccb8db71f8'
+album_id = 'ZPZ4RzN'
+access_token = 'f904fff532fee09f4144c6eab3e6f387f8c708b7'
+refresh_token = '42a4807e16cc5fecb19421ec9e5e65294781c707'
 
 # line bot key
-line_channel_access_token = 'YOUR_CHANNEL_ACCESS_TOKEN'
-line_channel_secret = 'YOUR_CHANNEL_SECRET'
+line_channel_access_token = 'Ie/Zd3EYV5q9mwQjAuEGe2xkjuBH25g0hlzzOu2B+WOFRrluCuvowKaZqmlAN+KMzQkYWA8yiFFjSPN+uuiTYMhU0HNioAv7PqNqTQk14oBZRD/aYQ46faf16HziOZghcxWuYjOXZIZM1jnBMMYdewdB04t89/1O/w1cDnyilFU='
+line_channel_secret = '2851d4db0aa44fce3cdfd5e471cf2f98'
