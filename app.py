9a380d4599ea504c649b1d0bd0f4bcc14cabd1a9
NightKirie <qwer55113322@gmail.com>
2018-05-20 15:55:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ from linebot.exceptions import (
 from linebot.models import *
 import tempfile, os
 from config import client_id, client_secret, album_id, access_token, refresh_token, line_channel_access_token, line_channel_secret
-import matplotlib.pyplot as pl
+from matplotlib import pyplot
+
 ##from matplotlib.gridspec import GridSpec
 
 ###above for import package
