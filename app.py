285964d706bda1b249dd55c5f07bfd4a7edfcd51
NightKirie <qwer55113322@gmail.com>
2018-05-20 20:41:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -134,7 +134,7 @@ def handle_message(event):
             line_bot_api.reply_message(
                 event.reply_token,[
                 TextSendMessage(text=message_content),
-                img])
+                image_message])
         except:
             line_bot_api.reply_message(
                 event.reply_token,
