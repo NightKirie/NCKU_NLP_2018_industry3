b9e3685719063fd6e4326e0565d3359db0254ef1
NightKirie <qwer55113322@gmail.com>
2018-05-20 14:04:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+import random
 from flask import Flask, request, abort
+from imgurpython import ImgurClient
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -6,18 +8,20 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
-)
+from linebot.models import *
+
+import tempfile, os
+from config import client_id, client_secret, album_id, access_token, refresh_token, line_channel_access_token, \
+    line_channel_secret
 
 app = Flask(__name__)
 
-# Channel Access Token
-line_bot_api = LineBotApi('Ie/Zd3EYV5q9mwQjAuEGe2xkjuBH25g0hlzzOu2B+WOFRrluCuvowKaZqmlAN+KMzQkYWA8yiFFjSPN+uuiTYMhU0HNioAv7PqNqTQk14oBZRD/aYQ46faf16HziOZghcxWuYjOXZIZM1jnBMMYdewdB04t89/1O/w1cDnyilFU=')
-# Channel Secret
-handler = WebhookHandler('2851d4db0aa44fce3cdfd5e471cf2f98')
+line_bot_api = LineBotApi(line_channel_access_token)
+handler = WebhookHandler(line_channel_secret)
 
-# 監聽所有來自 /callback 的 Post Request
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+
+
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -25,6 +29,7 @@ def callback():
 
     # get request body as text
     body = request.get_data(as_text=True)
+    # print("body:",body)
     app.logger.info("Request body: " + body)
 
     # handle webhook body
@@ -33,17 +38,71 @@ def callback():
     except InvalidSignatureError:
         abort(400)
 
-    return 'OK'
+    return 'ok'
 
 
-@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=(ImageMessage, TextMessage))
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(
-        event.reply_token,
-        message)
+    if isinstance(event.message, ImageMessage):
+        ext = 'jpg'
+        message_content = line_bot_api.get_message_content(event.message.id)
+        with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=ext + '-', delete=False) as tf:
+            for chunk in message_content.iter_content():
+                tf.write(chunk)
+            tempfile_path = tf.name
 
-import os
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+        dist_path = tempfile_path + '.' + ext
+        dist_name = os.path.basename(dist_path)
+        os.rename(tempfile_path, dist_path)
+        try:
+            client = ImgurClient(client_id, client_secret, access_token, refresh_token)
+            config = {
+                'album': album_id,
+                'name': 'Catastrophe!',
+                'title': 'Catastrophe!',
+                'description': 'Cute kitten being cute on '
+            }
+            path = os.path.join('static', 'tmp', dist_name)
+            client.upload_from_path(path, config=config, anon=False)
+            os.remove(path)
+            print(path)
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text='上傳成功'))
+        except:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text='上傳失敗'))
+        return 0
+
+    elif isinstance(event.message, VideoMessage):
+        ext = 'mp4'
+    elif isinstance(event.message, AudioMessage):
+        ext = 'm4a'
+    elif isinstance(event.message, TextMessage):
+        if event.message.text == "看看大家都傳了什麼圖片":
+            client = ImgurClient(client_id, client_secret)
+            images = client.get_album_images(album_id)
+            index = random.randint(0, len(images) - 1)
+            url = images[index].link
+            image_message = ImageSendMessage(
+                original_content_url=url,
+                preview_image_url=url
+            )
+            line_bot_api.reply_message(
+                event.reply_token, image_message)
+            return 0
+        else:
+            line_bot_api.reply_message(
+                event.reply_token, [
+                    TextSendMessage(text=' yoyo'),
+                    TextSendMessage(text='請傳一張圖片給我')
+                ])
+            return 0
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+
