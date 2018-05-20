4cc39a1d02d65876a9b3ea7cecd32f35c8f39453
NightKirie <qwer55113322@gmail.com>
2018-05-20 14:16:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,14 +9,8 @@ from linebot.exceptions import (
 )
 from linebot.models import *
 import tempfile, os
-from config import client_id, client_secret, album_id, access_token, refresh_token, line_channel_access_token, 
-    line_channel_secret
-import matplotlib.pyplot as pl
-pl.rcParams['font.sans-serif'] = ['Microsoft JhengHei']
-pl.rcParams['font.serif'] = ['Microsoft JhengHei']
-from matplotlib.gridspec import GridSpec
-import numpy
-from PIL import Image
+from config import client_id, client_secret, album_id, access_token, refresh_token, line_channel_access_token, line_channel_secret
+
 
 ###above for import package
 
