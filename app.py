3bafe2081031b5af735b80ecf65af9ce63594784
NightKirie <qwer55113322@gmail.com>
2018-05-20 21:07:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -109,10 +109,10 @@ def handle_message(event):
         ext = 'png'
         #message_content = line_bot_api.get_message_content(event.message.id)
         with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=ext + '-', delete=False) as tf:
-            for chunk in message_content.iter_content():
-                tf.write(chunk)
-            #imgggggg.save(tf, "PNG") 
-            #imgggggg.cloas()
+            ##for chunk in message_content.iter_content():
+               # tf.write(chunk)
+            imgggggg.save(tf, "PNG") 
+            imgggggg.close()
             tempfile_path = tf.name
 
         dist_path = tempfile_path + '.' + ext
