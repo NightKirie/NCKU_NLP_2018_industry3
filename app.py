a6163c38298a3425fdc948c76173e6be9dd104eb
NightKirie <qwer55113322@gmail.com>
2018-05-21 20:59:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -82,6 +82,7 @@ def handle_message(event):
                 line_bot_api.reply_message(
                     event.reply_token,[
                     TextSendMessage(text='以下是您所查詢的資料'),
+                    TextSendMessage(text=graphing.path),    
                     image_message])
             except:
                 line_bot_api.reply_message(
