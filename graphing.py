d61e25da9e81f828e027d912ce69208f421b7f6d
NightKirie <qwer55113322@gmail.com>
2018-05-22 00:01:20 +0800

    Update graphing.py

--- graphing.py
+++ graphing.py
@@ -14,7 +14,7 @@ from io import BytesIO
 '''
 import matplotlib.font_manager as font_manager
 
-path = "static/kaiu.ttf"
+path = "a.ttf"
 prop = font_manager.FontProperties(fname=path)
 pl.rcParams['font.family'] = prop.get_family()
 pl.rcParams['font.sans-serif'] = prop.get_name()
