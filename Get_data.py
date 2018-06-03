c11fda223cc21080932c1b665a57ccac90176975
NightKirie <qwer55113322@gmail.com>
2018-06-04 01:50:15 +0800

    import oauth2client

--- Get_data.py
+++ Get_data.py
@@ -1,5 +1,6 @@
 import graphing
 import gspread
+import oauth2client
 from oauth2client.service_account import ServiceAccountCredentials
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
