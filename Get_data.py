4433110e537ae3ef4428ffbc7749faaf261b9f59
NightKirie <qwer55113322@gmail.com>
2018-06-04 01:30:58 +0800

    add raw get_data

---
+++ Get_data.py
@@ -0,0 +1,5 @@
+import graphing
+
+def input(List):
+	return graphing.drawing1_1(
+                [["學校名稱", '科系名稱', '學生數'], ['台大', '資訊', 100], ['清大', '資訊', 70]])
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import graphing 
+import Get_data
 import time
 import random
 from flask import Flask, abort, request
