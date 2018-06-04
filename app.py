0cb4de76407aa8e4ab447c34597ea1bb393bd954
NightKirie <qwer55113322@gmail.com>
2018-06-04 11:18:54 +0800

    fix printimage error

--- app.py
+++ app.py
@@ -92,80 +92,80 @@ def handle_message(event):
                     PrintImage(img, event)
                 elif '教師數' in event.message.text:
                     img = Get_data.input([1, '台大', '清大', '資訊', 1, '教師數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
                 elif '上學年度畢業生數' in event.message.text:
                     img = Get_data.input([1, '台大', '清大', '資訊', 1, '上學年度畢業生數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
                 elif '106指考最低錄取分數' in event.message.text:
                     img = Get_data.input([1, '台大', '清大', '資訊', 1, '106指考最低錄取分數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
             elif '交大' in event.message.text:
                 if '學生數' in event.message.text:
                     img = Get_data.input([1, '台大', '交大', '資訊', 1, '學生數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
                 elif '教師數' in event.message.text:
                     img = Get_data.input([1, '台大', '交大', '資訊', 1, '教師數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
                 elif '上學年度畢業生數' in event.message.text:
                     img = Get_data.input([1, '台大', '交大', '資訊', 1, '上學年度畢業生數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
                 elif '106指考最低錄取分數' in event.message.text:
                     img = Get_data.input([1, '台大', '交大', '資訊', 1, '106指考最低錄取分數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
             elif '成大' in event.message.text:
                 if '學生數' in event.message.text:
                     img = Get_data.input([1, '台大', '成大', '資訊', 1, '學生數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
                 elif '教師數' in event.message.text:
                     img = Get_data.input([1, '台大', '成大', '資訊', 1, '教師數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
                 elif '上學年度畢業生數' in event.message.text:
                     img = Get_data.input([1, '台大', '成大', '資訊', 1, '上學年度畢業生數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
                 elif '106指考最低錄取分數' in event.message.text:
                     img = Get_data.input([1, '台大', '成大', '資訊', 1, '106指考最低錄取分數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
         elif '清大' in event.message.text:
             if '交大' in event.message.text:
                 if '學生數' in event.message.text:
                     img = Get_data.input([1, '清大', '交大', '資訊', 1, '學生數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
                 elif '教師數' in event.message.text:
                     img = Get_data.input([1, '清大', '交大', '資訊', 1, '教師數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
                 elif '上學年度畢業生數' in event.message.text:
                     img = Get_data.input([1, '清大', '交大', '資訊', 1, '上學年度畢業生數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
                 elif '106指考最低錄取分數' in event.message.text:
                     img = Get_data.input([1, '清大', '交大', '資訊', 1, '106指考最低錄取分數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
             elif '成大' in event.message.text:
                 if '學生數' in event.message.text:
                     img = Get_data.input([1, '清大', '成大', '資訊', 1, '學生數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
                 elif '教師數' in event.message.text:
                     img = Get_data.input([1, '清大', '成大', '資訊', 1, '教師數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
                 elif '上學年度畢業生數' in event.message.text:
                     img = Get_data.input([1, '清大', '成大', '資訊', 1, '上學年度畢業生數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
                 elif '106指考最低錄取分數' in event.message.text:
                     img = Get_data.input([1, '清大', '成大', '資訊', 1, '106指考最低錄取分數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
         elif '交大' in event.message.text:
             if '成大' in event.message.text:
                 if '學生數' in event.message.text:
                     img = Get_data.input([1, '交大', '成大', '資訊', 1, '學生數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
                 elif '教師數' in event.message.text:
                     img = Get_data.input([1, '交大', '成大', '資訊', 1, '教師數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
                 elif '上學年度畢業生數' in event.message.text:
                     img = Get_data.input([1, '交大', '成大', '資訊', 1, '上學年度畢業生數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
                 elif '106指考最低錄取分數' in event.message.text:
                     img = Get_data.input([1, '交大', '成大', '資訊', 1, '106指考最低錄取分數'])
-                    PrintImage(img)
+                    PrintImage(img, event)
         else:
             line_bot_api.reply_message(
                     event.reply_token,
