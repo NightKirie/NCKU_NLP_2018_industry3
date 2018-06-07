f966d6099d11ae283fd723829bb2df2b22a6f25a
Julian Fang <julianATA>
2018-06-07 22:33:25 +0800

    Data arrange

--- group1/analyser.py
+++ group1/analyser.py
@@ -4,6 +4,28 @@ import jieba
 with open('test1.json') as inputdata:
         data = json.load(inputdata)
 
-print(data)
-for rawdata in data:
-    print(rawdata)
+title = data['article_title'] 
+content = data['content']
+date = data['date']
+
+cnt = -1
+push_userid = " "
+
+CommentContent = {}
+CommentLevel = {}
+
+
+#Arrange comments + connect broken messages
+for mes in data['messages']:
+    if push_userid != mes['push_userid']:
+        cnt += 1
+        CommentContent[cnt] = mes['push_content']
+        CommentLevel[cnt] = mes['push_tag']
+        push_userid = mes['push_userid']
+    else:
+        CommentContent[cnt] = CommentContent[cnt]+mes['push_content']
+        CommentLevel[cnt] = mes['push_tag']
+
+for Com in CommentContent:
+    print(CommentContent[Com])
+    print(" ")
