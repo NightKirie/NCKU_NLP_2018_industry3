0cf417e7f694a8100916c82590c2cb102f80a0b7
JulianATA <julianisabear@gmail.com>
2018-06-24 16:35:49 +0800

    18/6/24

--- group1/analyser.py
+++ group1/analyser.py
@@ -1,29 +1,19 @@
 import json
 import jieba
-
-def ListCheck(arg0, arg1):
-    WordList =[]
-    ContextList = arg0
-    CheckList = arg1 
-    for Context in ContextList:
-        for Check in CheckList:
-            if Check[0] == Context:
-                WordList.append(Check)
-
-    return WordList
-
-test = []
-
 schools=[] 
 keyword =[]   
-
+department =[]
+database = {}
 for f in open("schools.txt", "r",encoding ='UTF-8'):
     schools.append(f.strip('\ufeff').strip('\n').split(' '))
 
 for f in open("keyword.txt", "r",encoding ='UTF-8'):
     keyword.append(f.strip('\ufeff').strip('\n').split(' '))
 
-with open('test1.json') as inputdata:
+for f in open("department.txt", "r",encoding ='UTF-8'):
+        department.append(f.strip('\ufeff').strip('\n').split(' '))
+
+with open('test0.json') as inputdata:
         data = json.load(inputdata)
 
 title = data['article_title'] 
@@ -35,7 +25,6 @@ push_userid = " "
 
 CommentContent = {}
 CommentLevel = {}
-
 
 #Arrange comments + connect broken messages
 for mes in data['messages']:
@@ -70,30 +59,62 @@ for seg in seg_list:
                if schooldict.get(sch[1],0) == 0:
                    schoolcount +=1
                    schooldict[sch[1]] = schnum
+                   database[sch[1]] = {}
 
+#print(database)
 print(schooldict)
 cnt = 0
+
+
+
 for Com in CommentContent:
     seg_list = jieba.cut(CommentContent[Com])
+    tag = []
     for seg in seg_list:
         for keyline in keyword:
             for key in keyline:
                 if seg == key:
-                    if tag_dict.get(str(Com), 0) == 0:
-                        tag_dict[str(Com)] = [keyline[0]]
-                    else:
-                        tag_dict[str(Com)] = [tag_dict[str(Com)] ,keyline[0]]
+                    tag.append(str(keyline[0]))
         for sch in schools:
             for name in sch:
                 if seg == name:
-                    if tag_dict.get(str(Com), 0) == 0:
-                        tag_dict[str(Com)] = sch[1]
-                    else:
-                        tag_dict[str(Com)] = [tag_dict[str(Com)] , sch[1]]
-                
-for tag in tag_dict:
-    print(tag,":",CommentContent[int(tag)],":",tag_dict[tag])
+                    tag.append(str(sch[1]))
+        for dep in department:
+            for name in dep:
+                if seg == name:
+                    tag.append(str(dep[0])+"系")
+    if tag != []:
+        tag_dict[str(Com)] = tag
 
-print("共",len(CommentContent),"回覆")
-print("共抽取",len(tag_dict),"份回復")
-print("抽取率",str(len(tag_dict)/len(CommentContent)*100),"%")
+
+for tag in tag_dict:              
+   print(tag_dict[tag])
+print("_________________")
+
+for tags in tag_dict:
+    sortedtag = []
+    for tag in tag_dict[tags]:
+        for sch in schools:
+            if tag == sch[1]:
+                sortedtag.append(str(tag))
+    for tag in tag_dict[tags]:
+        for dep in department:
+            if tag == (dep[0]+"系"):
+                sortedtag.append(str(tag))
+    for tag in tag_dict[tags]:
+        for key in keyword:
+            if tag == key[0]:
+                sortedtag.append(str(tag))
+    tag_dict[tags] = sortedtag
+
+for tag in tag_dict:              
+   print(tag_dict[tag])
+
+#for tag in tag_dict:
+#    print(tag,":",CommentContent[int(tag)],":",tag_dict[tag])
+
+#print("共",len(CommentContent),"回覆")
+#print("共抽取",len(tag_dict),"份回復")
+#print("抽取率",str(len(tag_dict)/len(CommentContent)*100),"%")
+
+#print(department)
