0a7ef68a5e68a5504a362a1caf592dfb6b6f27c5
NightKirie <qwer55113322@gmail.com>
2018-07-03 00:18:59 +0800

    Update output.py

--- output.py
+++ output.py
@@ -386,6 +386,9 @@ def output_api(list, line_bot_api, event):
     outputText = ""
     outputReply = []
     outputImageUrl = ""
+    if list is None:
+        outputReply.append(TextSendMessage(text='該搜尋條件不存在，不好意思。\n')
+        line_bot_api.reply_message(event.reply_token, outputReply)
     for listElement in list:
         output(listElement)
 
